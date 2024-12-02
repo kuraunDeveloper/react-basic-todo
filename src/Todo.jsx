f38e2a6b98f826d3a688148f6fc292e2eea04b81
kuraunDeveloper <kuraunDeveloper@gmail.com>
2024-12-02 14:40:16 +0900

    reactでTODOアプリを作ろう（第一回）

---
+++ src/Todo.jsx
@@ -0,0 +1,38 @@
+import "./styles.css";
+
+export const Todo = () => {
+    return (
+        <>
+            <div>
+                <input type="text" placeholder="TODOを入力" />
+            </div>
+            <div>
+                <p>未完了のTODO</p>
+                <ul>
+                    <li>
+                        <p>TODOです</p>
+                        <button>完了</button>
+                        <button>削除</button>
+                    </li>
+                    <li>
+                        <p>TODOです</p>
+                        <button>完了</button>
+                        <button>削除</button>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <p>完了のTODO</p>
+                <ul>
+                    <li>
+                        <p>TODOでした</p>
+                        <button>戻す</button>
+                    </li>
+                    <li>
+                        <p>TODOでした</p>
+                        <button>戻す</button>
+                    </li>
+                </ul></div>
+        </>
+    );
+};
